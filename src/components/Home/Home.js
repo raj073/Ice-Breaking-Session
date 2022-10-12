8ab3997210e8825a053cceb9fe05779826df3e77
raj073 <rajoni.das@outlook.com>
2022-10-12 18:24:09 +0600

    Carousel updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,8 +23,8 @@ const Home = () => {
                         alt="MCQ Quiz Exam"
                     />
                     <Carousel.Caption>
-                        <h3>MCQ Quiz Exam</h3>
-                        <p>Widely used in MCQ Examination, Surveys, Assessments and Recruitment, Optical Mark Recognition (OMR)</p>
+                        <h3>Ice-Breaking Session</h3>
+                        <p> Ice-breaking is an activity that is conducted when a group of unknown or semi-known or little-known people gathers in a particular place for receiving training or attending a workshop or similar purpose</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -36,7 +36,7 @@ const Home = () => {
 
                     <Carousel.Caption>
                         <h3>Rubik's Cube</h3>
-                        <p>Magic Cube, Speed Cube, Puzzle Cube, Cube</p>
+                        <p>Magic Cube, Speed Cube, Puzzle Cube, Cube <br />The Rubik's Cube is a 3-D combination puzzle originally invented in 1974 by Hungarian sculptor and professor of architecture Ernő Rubik. Originally called the Magic Cube, the puzzle was licensed by Rubik to be sold by Ideal Toy Corp in 1980 via businessman Tibor Laczi and Seven Towns founder Tom Kremer. </p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -49,7 +49,7 @@ const Home = () => {
                     <Carousel.Caption>
                         <h3>Standardized Test</h3>
                         <p>
-                            A standardized test is a test that is administered and scored in a consistent, or "standard", manner
+                            A standardized test is a test that is administered and scored in a consistent, or "standard", manner. Standardized tests are designed in such a way that the questions and interpretations are consistent and are administered and scored in a predetermined, standard manner.
                         </p>
                     </Carousel.Caption>
                 </Carousel.Item>
@@ -61,7 +61,7 @@ const Home = () => {
                     />
                     <Carousel.Caption>
                         <h3>Working as a Team</h3>
-                        <p>A successful team is one where everyone's unique skills and strengths help the team achieve a shared goal in the most effective way.</p>
+                        <p>A successful team is one where everyone's unique skills and strengths help the team achieve a shared goal in the most effective way. <br />Teamwork is, “The process of working collaboratively with a group of people in order to achieve a goal.Teamwork is often a crucial part of a business, as it is often necessary for colleagues to work well together, trying their best in any circumstance. </p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
